36cc880a8607f0fa29cc18e751cd579073cd3e44
Mathéo Romano <romano.matheo@gmail.com>
2026-06-05 14:38:31 +0200

    [MRO] Fonctions d'affichage

--- Gaphic/Output.py
+++ Gaphic/Output.py
@@ -1,5 +1,6 @@
 """
-Devrait - on  le mettre dans Tools ? mail ?
+Devrait - on  le mettre dans Tools ? main ?
+Fonction d'affichage pour le projet
 """
 
 import colorama
@@ -14,3 +15,13 @@ def printRed(a):
 
 def printGreen(a):
     print(GREEN+a+END,end='')
+
+def AfficherMotus(mot: str, comparaison: list[int]):
+    """
+    Affiche le résultat d'une proposition de mot pour le jeu MOTUS
+
+    param :
+        - mot : mot à deviner
+        - comparaison : liste des résultats de la comparaison (0 = incorrect, 1 = présent, 2 = correct)
+    """
+    pass
