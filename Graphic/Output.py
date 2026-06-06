7a4561c7a3ef4a411fbcd4399cd6929c418aedf1
Marc Roussel <marc.roussel@universite-paris-saclay.fr>
2026-06-06 16:50:02 +0200

    [MARC] Ajout de AfficherMotus + test

--- Graphic/Output.py
+++ Graphic/Output.py
@@ -10,11 +10,16 @@ RED = colorama.Back.RED
 GREEN = colorama.Back.GREEN
 END = colorama.Back.RESET
 
+# ----- Fonctions d'affichage coloré ----- #
+def printDefault(a):
+    print(a, end='')
+
 def printRed(a):
     print(RED+a+END,end='')
 
 def printGreen(a):
     print(GREEN+a+END,end='')
+# ---------------------------------------- #
 
 def AfficherMotus(w: str, comparaison: list[int]):
     """
@@ -24,4 +29,19 @@ def AfficherMotus(w: str, comparaison: list[int]):
         - w : mot du joueur
         - comparaison : liste des résultats de la comparaison (0 = incorrect, 1 = présent, 2 = correct)
     """
-    pass
+    if len(comparaison) != len(w) : raise ValueError("AfficherMotus(w, comparaison) :\n Error : w has not same length as comparaison")
+
+    for i in range(len(w)):
+
+        # Pour les lettres incorrectes
+        if comparaison[i] == 0:
+            printDefault(w[i])
+
+        # Pour les lettres présentes
+        if comparaison[i] == 1:
+            printGreen(w[i])
+
+        # Pour les lettres correctes
+        if comparaison[i] == 2:
+            printRed(w[i])
+    print()
--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 import Motus.Tools.RandomWord as rw
 import Motus.Tools.Rechercher as rech
 import Motus.TxtLoader.DataLoader as datas
+import Graphic.Output as out
 
 def __main__():
     """
@@ -12,12 +13,12 @@ def __main__():
     """
 
     # Test de chargement du dictionnaire 
-    
     print("Chargement du dico")
     dico = datas.loadDico('datas/scrabble.txt')
 
     print("Le nouveau mot de longueur 5 est : " + rw.randomWordFromDico(dico=dico, l=5))
     print("Le nouveau mot de longueur 7 est : " + rw.randomWordFromDico(dico=dico, l=7))
+
 
     # Test de inside
     print("Test de inside: ", + rech.inside("aaaa", dico[4])) # Doit renvoyer 0
@@ -26,7 +27,11 @@ def __main__():
     print("Test de inside: ", + rech.inside("pris", dico[4])) # Doit renvoyer 1
     print("Test de inside: ", + rech.inside("gkmx", dico[4])) # Doit renvoyer 0
 
-    # return
+
+    # Test de AfficherMotus
+    mot = "abcdef"
+    comparaison_test = [2, 1, 1, 0, 2, 0] 
+    out.AfficherMotus(mot, comparaison_test)
 
 if __name__ == "__main__":
     __main__()
