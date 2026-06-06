5e5bff394e95b1011d9577b83cdbdee8a4ee5d80
Marc Roussel <marc.roussel@universite-paris-saclay.fr>
2026-06-06 14:03:08 +0200

    [MARC] Exos 1.2 et 1.3 avec tests sur main

--- Modus/TxtLoader/DataLoader.py
+++ Modus/TxtLoader/DataLoader.py
@@ -1,6 +1,3 @@
-"""
-A completer
-"""
 def loadDico(path : str) -> dict[int, list[str]]:
     """
     Charge un dictionnaire de mots à partir d'un fichier texte et les trie par longueur.
@@ -12,4 +9,22 @@ def loadDico(path : str) -> dict[int, list[str]]:
     return:
         Un dictionnaire où les clés sont les longueurs des mots et les valeurs sont des listes de mots de cette longueur
     """
-    return
+    
+    dico_file = open(path, 'r')
+
+    dico = {}
+    for word in dico_file:
+        if word.strip() is not None:
+
+            l = len(word.strip())
+
+            # Si le tableau des mots de longueur l n'existe pas
+            # On le crée
+            if l not in dico.keys():
+                dico[l] = [word.strip()]
+
+            else:
+                dico[l].append(word.strip())
+
+    dico_file.close()
+    return dico
