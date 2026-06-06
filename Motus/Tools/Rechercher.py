82f611c1676c1abc7697295d00476316446781e3
Marc Roussel <marc.roussel@universite-paris-saclay.fr>
2026-06-06 15:40:20 +0200

    [MARC] Ajout de la fonction inside + test

--- Motus/Tools/Rechercher.py
+++ Motus/Tools/Rechercher.py
@@ -1,18 +1,35 @@
-"""
-
-"""
-
-def inside(w : str , l : list[str]) -> bool:
+def inside(w : str , lst : list[str]) -> bool:
     """
     Recherche dichotomique d'un mot dans une liste de chaînes de caractères triée par ordre alphabétique
      Complexite :
-        - Temps : O(log( |n_l| ))
+        - Temps : O(log( |n_lst| ))
     param :
         w: le mot à chercher
-        l: la liste de chaînes de caractères triée par ordre alphabétique
+        lst: la liste de chaînes de caractères triée par ordre alphabétique
     return:
-        True si "w" est présente dans "l" et False sinon
+        True si "w" est présente dans "lst" et False sinon
     """
-    return
 
+    # Cas de base où la liste est vide 
+    if len(lst) == 0:
+        return False
 
+    # Calcul de l'indice du milieu
+    middle = len(lst)//2
+
+    # Cas de base où le mot a été trouvé
+    if lst[middle] == w:
+        return True
+    
+    # Cas de base où le mot n'a pas été trouvé 
+    # et le tableau n'a qu'un élément (milieu = 0)
+    elif middle == 0:
+        return False
+    
+    # Recherche dans les mots à gauche du milieu
+    elif lst[middle] > w:
+        return inside(w, lst[0 : middle])
+
+    # Recherche dans les mots à droite du milieu
+    elif lst[middle] < w:
+        return inside(w, lst[middle+1 : len(lst)])
--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 """
 
 import Motus.Tools.RandomWord as rw
+import Motus.Tools.Rechercher as rech
 import Motus.TxtLoader.DataLoader as datas
 
 def __main__():
@@ -17,6 +18,13 @@ def __main__():
 
     print("Le nouveau mot de longueur 5 est : " + rw.randomWordFromDico(dico=dico, l=5))
     print("Le nouveau mot de longueur 7 est : " + rw.randomWordFromDico(dico=dico, l=7))
+
+    # Test de inside
+    print("Test de inside: ", + rech.inside("aaaa", dico[4])) # Doit renvoyer 0
+    print("Test de inside: ", + rech.inside("amer", dico[4])) # Doit renvoyer 1
+    print("Test de inside: ", + rech.inside("mise", dico[4])) # Doit renvoyer 1
+    print("Test de inside: ", + rech.inside("pris", dico[4])) # Doit renvoyer 1
+    print("Test de inside: ", + rech.inside("gkmx", dico[4])) # Doit renvoyer 0
 
     # return
 
